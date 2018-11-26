4011a998c8b67188e4502e0051495255b10bb61f
Peter Dietrich <xosofox@gmail.com>
2018-11-26 17:57:15 +0100

    Cleanup and rotate

--- js/main.js
+++ js/main.js
@@ -3,41 +3,55 @@ var DEFAULT_MAX = 6;
 var rolls = 1;
 
 function rollMain() {
-	render($('#mainDie'),roll())
+    var die = document.getElementById('mainDie');
+    die.classList.add('rolling');
+    var rolling = true;
+    setTimeout(function() {
+        console.log('Roll false');
+        rolling = false;
+        die.classList.remove('rolling');
+    }, 500);
+    var interval = setInterval(function() {
+        render(die, roll());
+
+        if (rolling === false) {
+            clearInterval(interval);
+        }
+    }, 50);
 }
 
 function roll(min, max) {
-	min = min || DEFAULT_MIN;
-	max = max || DEFAULT_MAX;
-	var res = getRandomInt(min,max);
-	return res;
+    min = min || DEFAULT_MIN;
+    max = max || DEFAULT_MAX;
+    var res = getRandomInt(min, max);
+    return res;
 }
 
 //http://stackoverflow.com/questions/1527803/generating-random-numbers-in-javascript-in-a-specific-range
-function getRandomInt (min, max) {
+function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function render(div,num) {
-	console.log(div, num);
-	var pips = pipSetup[num];
-	for (var pip=1;pip<=7;pip++) {
-		if (pips.indexOf(pip)>=0) {
-			div.find(".pip"+pip).show();
-		} else {
-			div.find(".pip"+pip).hide();
-		}
-	}
+function render(div, num) {
+    var pips = pipSetup[num];
+    for (var pip = 1; pip <= 7; pip++) {
+        var p = document.getElementsByClassName('pip' + pip)[0];
+        if (pips.indexOf(pip) >= 0) {
+            p.style.display = 'block';
+        } else {
+            p.style.display = 'none';
+        }
+    }
 }
 
 // 1 2
 // 345
 // 6 7
-var pipSetup=[0,
+var pipSetup = [0,
     [4],
-    [1,7],
-    [1,4,7],
-    [1,2,6,7],
-    [1,2,4,6,7],
-    [1,2,3,5,6,7]
+    [1, 7],
+    [1, 4, 7],
+    [1, 2, 6, 7],
+    [1, 2, 4, 6, 7],
+    [1, 2, 3, 5, 6, 7]
 ];
